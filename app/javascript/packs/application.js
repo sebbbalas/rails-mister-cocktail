c8dc5ba3372ff31e7ab8d61464b6b1aced1b47fe
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-15 12:02:37 +0100

    models done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,5 +6,9 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
 
 console.log('Hello World from Webpacker')
